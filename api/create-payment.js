878878cfa9ade6849ad43eb289de28c07eb4de5c
ACSTECN <alessandrochristossimonis1@gmail.com>
2026-06-27 15:59:37 -0300

    Remove contatos e atualiza planos para mobile

--- api/create-payment.js
+++ api/create-payment.js
@@ -23,8 +23,8 @@ module.exports = async function handler(req, res) {
         }
 
         const planDetails = {
-            starter: { title: 'Fitbory Starter', price: 0.20 },
-            premium: { title: 'Fit Bory Premium', price: 0.30 }
+            starter: { title: 'Fitbory Starter', price: 14.90 },
+            premium: { title: 'Fit Bory Premium', price: 39.90 }
         };
         const selectedPlan = planDetails[plan];
         if (!selectedPlan) {
