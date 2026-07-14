fdfc80447b5e54a7b52406eee1462166145bb8cc
ACSTECN <alessandrochristossimonis1@gmail.com>
2026-07-14 11:39:05 -0300

    Atualiza valores dos planos

--- api/create-payment.js
+++ api/create-payment.js
@@ -1,5 +1,5 @@
 const PLAN_DETAILS = {
-    starter: { title: 'Fitbory Starter', price: 2.50 },
+    starter: { title: 'Fitbory Starter', price: 5.00 },
     premium: { title: 'Fit Bory Premium', price: 3.0 }
 };
 
